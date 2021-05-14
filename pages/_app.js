774f252d6ade700390ecef7b701b8fce7246629b
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-05-13 23:01:17 -0300

    style home

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
-import { AuthProvider } from './Contexts/AuthContext';
 import Head from "next/head";
-
+import { AuthProvider } from './Contexts/AuthContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </>
     );
 }
