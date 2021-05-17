93679f341f61f1f5cb7e0f5cae079a4e9cdb5023
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-05-17 17:39:29 -0300

    cookies

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,20 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
 import Head from "next/head";
 import { AuthProvider } from './Contexts/AuthContext';
+import { CookiesProvider } from 'react-cookie';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
       </Head>
+      <CookiesProvider>
           <AuthProvider>
               <Component {...pageProps} />
           </AuthProvider>
+      </CookiesProvider>
     </>
     );
 }
