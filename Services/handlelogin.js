9d5188ef514722017fd5b98ca0a296150ce9a32f
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-05-23 23:18:15 -0300

    fix

--- Services/handlelogin.js
+++ Services/handlelogin.js
@@ -6,7 +6,7 @@ export const handleLogin = async (email, password) => {
         const user = await firebase.auth().signInWithEmailAndPassword(email, password)
         const user_uid = await user.user.uid
         const url = process.env.MY_URL + "/api/auth"
-console.log("url", url)
+
         const res = await fetch( url, {
             method: 'POST',
             mode: 'cors',
@@ -18,7 +18,7 @@ console.log("url", url)
         })
 
         const token = await res.json()
-        console.log("token",token)
+
         return (token.authToken)
         
     } catch (error) {
