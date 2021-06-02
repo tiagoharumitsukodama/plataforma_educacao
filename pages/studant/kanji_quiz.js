8f471421b0b9711ba4421882b2050b0533c5e97f
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-06-01 23:18:28 -0300

    create kanji quiz

---
+++ pages/studant/kanji_quiz.js
@@ -0,0 +1,48 @@
+import styles from '../../styles/Studant.module.css';
+import firebase from '../../Repositories/firebase';
+import Nav from '../../Layouts/nav';
+import Menu from '../../Components/Studant/menu'
+import { parseCookies } from '../../Services/parseCookies'
+import { Card } from 'react-bootstrap'
+
+export default function Studant(props){
+
+  const user_name = props.props.user_name
+  if( !props.props.user_name )
+  console.log(`sem dados`)
+
+  return (
+      <div className={styles.container}>
+  
+        <Nav user={user_name}/>
+  
+        <main className={styles.main}>
+          Kanji quiz
+        </main>
+      </div>
+  );
+}
+
+
+Studant.getInitialProps = async (ctx) => {
+  try {
+    const {authToken} = parseCookies(ctx.req)
+
+    if( !authToken )
+      throw new Error('Can not find user')
+    
+    const credentialUser = await firebase.auth().signInWithCustomToken(authToken)
+    const user = credentialUser.user.displayName
+
+      return {
+        props: {
+          user_name: user
+        }
+      }
+      
+    } catch (error) {
+        const res = ctx.res
+        res.writeHead(307, { Location: '/login' })
+        res.end()
+    }
+  }
